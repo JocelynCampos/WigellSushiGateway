3fd09596b50bb1b3f336d8277f82c54f53cf1db8
Jocelyn <aracely.campos@hotmail.com>
2025-10-12 13:06:30 +0200

    Creating gateway

---
+++ src/main/java/se/edugrade/wigellsushigateway/WSushiGatewayApplication.java
@@ -0,0 +1,13 @@
+package se.edugrade.wigellsushigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WSushiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WSushiGatewayApplication.class, args);
+	}
+
+}
